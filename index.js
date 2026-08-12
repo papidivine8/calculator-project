b85cc045adb74da543dad68593396d9ecf2a389d
papidivine8 <divinechilaka8@gmail.com>
2026-08-12 10:07:11 +0100

    JS added

---
+++ index.js
@@ -0,0 +1,13 @@
+//const numberInput = document.querySelectorAll('.nums');
+
+//
+
+function add (num1, num2) {
+    return Number(num1 + num2);
+}
+
+function subtract (num1, num2) {
+    return Number(num1 - num2);
+}
+
+console.log(subtract(5, 7))
