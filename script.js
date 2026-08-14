028a7755c9d2754a37a3d984e9bbeaaf41ade478
papidivine8 <divinechilaka8@gmail.com>
2026-08-14 09:43:57 +0100

    symbols display js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let numberInput = document.querySelectorAll(".nums");
-let finalDisplay = document.getElementById("final-display");
+const numberInput = document.querySelectorAll(".nums");
+const symbols = document.querySelectorAll(".operation-symbol")
+const finalDisplay = document.getElementById("final-display");
+
+/*let num1 = 0;
+let num2 = 0;*/
+
+let total = null;
+
 
 numberInput.forEach(num => {
     num.addEventListener("click", (event) => {
@@ -9,8 +16,15 @@ numberInput.forEach(num => {
     })
 })
 
-function add (num1, num2) {
-    return Number(num1 + num2);
+symbols.forEach(symbol => {
+    symbol.addEventListener("click", (event) => {
+        const outputValue = event.target.textContent;
+        finalDisplay.textContent += outputValue;
+    })
+})
+
+function sumAll (...inputs) {
+    total = inputs.reduce((total, current) => total + current, 0)
 }
 
 function subtract (num1, num2) {
